af9c811fea88769151f92e385c759c24f93b4d85
Kevin Tatagiba <kevin.tatagiba@joinplank.com>
2025-12-08 17:47:40 -0300

    chore: reset next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  output: 'export',
-}
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
